663b7c56c5263aeb71371436011c098572f2a403
Kirill Rudenko <slimpkowl@gmail.com>
2024-11-27 16:04:15 +0700

    feat: add department in models

--- src/models/models.py
+++ src/models/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from sqlalchemy import (
     Boolean,
     ForeignKey,
@@ -7,6 +8,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy_utils import Ltree
 
 Base = declarative_base()
 
@@ -25,6 +27,9 @@ class User(Base):
     company: Mapped["Company"] = relationship("Company", back_populates="employees")
     position_id: Mapped[int] = mapped_column(ForeignKey("position.id"), nullable=True)
     position: Mapped["Position"] = relationship("Position", back_populates="users")
+    department_id: Mapped[Optional[int]] = mapped_column(ForeignKey("departments.id"), nullable=True)
+    department: Mapped["Department"] = relationship("Department", back_populates="employees")
+    managed_departments: Mapped[list["Department"]] = relationship("Department", back_populates="manager")
 
 
 class Company(Base):
@@ -33,6 +38,7 @@ class Company(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String, unique=True, index=True)
     employees: Mapped[list["User"]] = relationship("User", back_populates="company")
+    departments: Mapped[list["Department"]] = relationship("Department", back_populates="company")
 
 
 class Position(Base):
@@ -59,3 +65,17 @@ class Invite(Base):
     token: Mapped[str] = mapped_column(String, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
     company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
+
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    path: Mapped[str] = mapped_column(Ltree, index=True)
+    company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
+    manager_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"), nullable=True)
+
+    company: Mapped["Company"] = relationship("Company", back_populates="departments")
+    manager: Mapped[Optional["User"]] = relationship("User", back_populates="managed_departments")
+    employees: Mapped[list["User"]] = relationship("User", back_populates="department")
